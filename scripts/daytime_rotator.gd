715993a46f61fde01a7d3392f70556632889bb41
Jeppe Lindberg <jepp3441@gmail.com>
2025-11-03 12:57:27 +0100

    progress

--- scripts/daytime_rotator.gd
+++ scripts/daytime_rotator.gd
@@ -4,14 +4,14 @@ extends Node3D
 
 @onready var main = get_node('/root/main')
 
-var base_rot_z = 0.0
+var base_rot_y = 0.0
 
 
 func _ready():
-	base_rot_z = rotation_degrees.z
+	base_rot_y = rotation_degrees.y
 
 func _process(_delta: float) -> void:
-	rotation_degrees.z = base_rot_z + (main.hour_of_day() / 24.0 * 360)
+	rotation_degrees.y = base_rot_y + (main.hour_of_day() / 24.0 * 360)
 
 
 
